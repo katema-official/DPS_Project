86ba3bb7d63aef5a8a087ecb60d2157571fc3dcc
katema-official <alessio.lagreca13@gmail.com>
2022-05-12 22:04:04 +0200

    Tested seta and MQTT subscriber

--- src/main/java/alessio_la_greca_990973/smart_city/taxi/SubscriberExample.java
+++ src/main/java/alessio_la_greca_990973/smart_city/taxi/SubscriberExample.java
@@ -1,13 +1,76 @@
 package alessio_la_greca_990973.smart_city.taxi;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import alessio_la_greca_990973.seta.RideRequestThread;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.eclipse.paho.client.mqttv3.*;
+import ride.request.RideRequestMessageOuterClass.RideRequestMessage;
+
+import java.sql.Timestamp;
+import java.util.Scanner;
 
 public class SubscriberExample {
     public static void main(String[] args) {
         MqttClient client;
         String broker = "tcp://localhost:1883";
         String clientId = MqttClient.generateClientId();
-        String topic = "home/controllers/temp";
+        String topic = "seta/smartcity/rides/+";
         int qos = 2;
+
+        try {
+            client = new MqttClient(broker, clientId);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+
+            // Connect the client
+            System.out.println(clientId + " Connecting Broker " + broker);
+            client.connect(connOpts);
+            System.out.println(clientId + " Connected");
+
+
+            // Callback
+            client.setCallback(new MqttCallback() {
+
+                public void messageArrived(String topic, MqttMessage message) throws InvalidProtocolBufferException {
+                    // Called when a message arrives from the server that matches any subscription made by the client
+                    String time = new Timestamp(System.currentTimeMillis()).toString();
+                    RideRequestMessage rrm = RideRequestMessage.parseFrom(message.getPayload());
+                    String receivedMessage = rrm.getId() + " - (" + rrm.getStartingX() + "," +rrm.getStartingY() + ") - (" + + rrm.getArrivingX() + "," +rrm.getArrivingY() + ")";
+                            System.out.println(clientId + " Received a Message! - Callback - Thread PID: " + Thread.currentThread().getId() +
+                            "\n\tTime:    " + time +
+                            "\n\tTopic:   " + topic +
+                            "\n\tMessage: " + receivedMessage +
+                            "\n\tQoS:     " + message.getQos() + "" +
+                            "\n");
+
+                }
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println(clientId + " Connectionlost! cause:" + cause.getMessage() + "-  Thread PID: " + Thread.currentThread().getId());
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    // Not used here
+                }
+
+            });
+            System.out.println(clientId + " Subscribing ... - Thread PID: " + Thread.currentThread().getId());
+            client.subscribe(topic, qos);
+            System.out.println(clientId + " Subscribed to topics : " + topic);
+
+
+            System.out.println("\n ***  Press a random key to exit *** \n");
+            Scanner command = new Scanner(System.in);
+            command.nextLine();
+            client.disconnect();
+
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+
     }
 }
